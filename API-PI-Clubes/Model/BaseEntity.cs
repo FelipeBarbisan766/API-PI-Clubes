b253019a6238bbd2dd0463d2412bd01c8780fd11
FelipeBarbisan766 <felipebarbisan76@gmail.com>
2026-03-05 22:40:48 -0300

    feat: start clube controller and create objVO of address

---
+++ API-PI-Clubes/Model/BaseEntity.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API_PI_Clubes.Model
+{
+    public class BaseEntity
+    {
+        [Key]
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public bool IsActive { get; set; } = true;
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime? UpdatedAt { get; set; }
+    }
+}
--- API-PI-Clubes/Model/Clube.cs
+++ API-PI-Clubes/Model/Clube.cs
@@ -1,10 +1,12 @@
-﻿namespace API_PI_Clubes.Model
+﻿using API_PI_Clubes.Model.ValueObjects;
+
+namespace API_PI_Clubes.Model
 {
-    public class Clube
+    public class Clube : BaseEntity
     {
-        public int Id { get; set; }
+        //public Clube(string name, AddressVO address, int phoneNumber, string description) { Name = name; Address = address; PhoneNumber = phoneNumber; Description = description; }
         public string Name { get; set; }
-        public string Address { get; set; }
+        public AddressVO Address { get; set; }
         public int PhoneNumber { get; set; }
         public string Description { get; set; }
     }

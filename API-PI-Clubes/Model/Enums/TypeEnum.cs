e6c5482bb4f093dbe05c825146e554f9ffb6892e
FelipeBarbisan766 <felipebarbisan76@gmail.com>
2026-03-06 22:07:09 -0300

    feat: created entity "quadra" and yours enums

---
+++ API-PI-Clubes/Model/Enums/TypeEnum.cs
@@ -0,0 +1,10 @@
+﻿namespace API_PI_Clubes.Model.Enums
+{
+    public enum TypeEnum
+    {
+        None = 0,
+        futebol = 1,
+        tennis = 2,
+        basket = 3,
+    }
+}
---
+++ API-PI-Clubes/Model/Quadra.cs
@@ -0,0 +1,19 @@
+﻿using API_PI_Clubes.Model.Enums;
+
+namespace API_PI_Clubes.Model
+{
+    public class Quadra : BaseEntity
+    {
+        public Quadra() { }
+
+        public string Name { get; set; }
+        public TypeEnum Type { get; set; }
+        public SurfaceEnum Surface { get; set; }
+        public bool IsCovered { get; set; }
+        public double PricePerHour { get; set; }
+        public string Description { get; set; }
+
+        //public Clube Clube { get; set; }
+
+    }
+}

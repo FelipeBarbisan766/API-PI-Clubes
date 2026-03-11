a4e3cdac0b7c51e5c3a03f86bd46b882f9976f28
FelipeBarbisan766 <felipebarbisan76@gmail.com>
2026-03-11 08:27:31 -0300

    fix: changed type of phoneNumber

--- API-PI-Clubes/Model/Clube.cs
+++ API-PI-Clubes/Model/Clube.cs
@@ -7,7 +7,7 @@ namespace API_PI_Clubes.Model
         //public Clube(string name, AddressVO address, int phoneNumber, string description) { Name = name; Address = address; PhoneNumber = phoneNumber; Description = description; }
         public string Name { get; set; }
         //public AddressVO Address { get; set; }
-        public int PhoneNumber { get; set; }
+        public string PhoneNumber { get; set; }
         public string Description { get; set; }
     }
 }
--- API-PI-Clubes/Model/DTOs/ClubeDTO.cs
+++ API-PI-Clubes/Model/DTOs/ClubeDTO.cs
@@ -4,14 +4,14 @@
     public class CreateClubeDTO
     {
         public string Name { get; set; }
-        public int PhoneNumber { get; set; }
+        public string PhoneNumber { get; set; }
         public string Description { get; set; }
     }
 
     public class UpdateClubeDTO
     {
         public string Name { get; set; }
-        public int PhoneNumber { get; set; }
+        public string PhoneNumber { get; set; }
         public string Description { get; set; }
     }
 

0b2bc131183c99ea0d59ce104465ea3a8aba9d93
FelipeBarbisan766 <felipebarbisan76@gmail.com>
2026-03-14 21:21:26 -0300

    feat: created userEntity

---
+++ API-PI-Clubes/Model/User.cs
@@ -0,0 +1,10 @@
+﻿namespace API_PI_Clubes.Model
+{
+    public class User : BaseEntity
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}

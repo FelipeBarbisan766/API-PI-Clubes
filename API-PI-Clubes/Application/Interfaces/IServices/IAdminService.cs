2d36d1ffd32129cffb1833618d48e01ab61dba8f
felipebarbisan766 <felipebarbisan76@gmail.com>
2026-05-04 08:32:07 -0300

    feat: created admin/me to get admin info

--- API-PI-Clubes/Application/Interfaces/IServices/IAdminService.cs
+++ API-PI-Clubes/Application/Interfaces/IServices/IAdminService.cs
@@ -1,11 +1,12 @@
-﻿using API_PI_Clubes.Application.DTOs;
+﻿using System.Security.Claims;
+using API_PI_Clubes.Application.DTOs;
 
 namespace API_PI_Clubes.Application.Interfaces.IServices
 {
     public interface IAdminService
     {
         Task<ResponseAdminDTO> GetById(Guid id);
-        Task<ResponseAdminDTO> GetByUserId(Guid id);
+        Task<ResponseAdminDTO> GetCurrentUserInfo(ClaimsPrincipal user);
         Task<ResponseIdDTO> Create(CreatAdminDTO dto);
         Task<ResponseAdminDTO> Update(Guid id, UpdateAdminDTO dto);
         Task Delete(Guid id);

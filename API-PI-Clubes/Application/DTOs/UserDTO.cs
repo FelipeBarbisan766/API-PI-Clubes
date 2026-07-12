eacd510bba9d41de63a7abd651661ffde12f41e4
felipebarbisan766 <felipebarbisan76@gmail.com>
2026-07-11 21:03:39 -0300

    feat: Small change in the update user

--- API-PI-Clubes/Application/DTOs/UserDTO.cs
+++ API-PI-Clubes/Application/DTOs/UserDTO.cs
@@ -11,10 +11,8 @@ namespace API_PI_Clubes.Application.DTOs
 
     public class UpdateUserDTO
     {
-        public string Name { get; set; }
-        public string Email { get; set; }
-        public string PhoneNumber { get; set; }
-
+        public string? Name { get; set; }
+        public string? PhoneNumber { get; set; }
     }
 
     public class ResponseUserDTO

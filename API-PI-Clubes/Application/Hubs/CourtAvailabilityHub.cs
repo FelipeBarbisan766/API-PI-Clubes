9e775d49b29ff016100ca73da3b7ccd4eb9e001a
felipebarbisan766 <felipebarbisan76@gmail.com>
2026-08-08 17:36:45 -0300

    fix: changed nivel access in hub

--- API-PI-Clubes/Application/Hubs/CourtAvailabilityHub.cs
+++ API-PI-Clubes/Application/Hubs/CourtAvailabilityHub.cs
@@ -4,19 +4,17 @@ using Microsoft.AspNetCore.SignalR;
 
 namespace API_PI_Clubes.Hubs
 {
-    [Authorize]
+    [AllowAnonymous]
     public class CourtAvailabilityHub : Hub<ICourtAvailabilityHubClient>
     {
         public async Task JoinClubGroup(Guid clubId)
         {
             await Groups.AddToGroupAsync(Context.ConnectionId, GroupName(clubId));
         }
-
         public async Task LeaveClubGroup(Guid clubId)
         {
             await Groups.RemoveFromGroupAsync(Context.ConnectionId, GroupName(clubId));
         }
-
         private static string GroupName(Guid clubId) => $"club-{clubId}";
     }
 }
